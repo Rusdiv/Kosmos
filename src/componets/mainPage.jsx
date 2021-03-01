5583998fb0df2b646869a647c673250f61e13bd4
Rusdiv <64499427+Rusdiv@users.noreply.github.com>
2021-03-01 21:39:38 +0300

    Change mainPage , add buttons and me

--- src/componets/mainPage.jsx
+++ src/componets/mainPage.jsx
@@ -3,11 +3,26 @@ import { makeStyles } from "@material-ui/core/styles";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import Preloader from "./Preloader";
+import { Avatar, Badge, Fab } from "@material-ui/core";
+import { deepPurple } from "@material-ui/core/colors";
+import FavoriteIcon from "@material-ui/icons/Favorite";
 
 const useStyles = makeStyles((theme) => ({
   list: {
     width: 360,
-    backgroundColor: theme.palette.background.paper,
+    backgroundColor: "#eaeaea",
+  },
+  purple: {
+    color: theme.palette.getContrastText(deepPurple[500]),
+    backgroundColor: deepPurple[500],
+  },
+  fab: {
+    position: "absolute",
+    bottom: theme.spacing(2),
+    right: theme.spacing(2),
+  },
+  name: {
+    marginLeft: theme.spacing(2),
   },
 }));
 
@@ -18,7 +33,7 @@ function ListItemLink(props) {
 export default function MainPage(props) {
   const classes = useStyles();
   const [preloader, setPreloader] = useState(true);
-
+  const [button, setButton] = useState(true);
   setTimeout(() => {
     setPreloader(false);
   }, 2000);
@@ -28,6 +43,10 @@ export default function MainPage(props) {
       {preloader && <Preloader />}
       {!preloader && (
         <div>
+          <Badge variant="dot" color="primary">
+            <Avatar className={classes.purple}>RL</Avatar>
+          </Badge>
+          <span className={classes.name}>Ruslan Lukoanov</span>
           <h2>Космос</h2>
           <List
             className={classes.list}
@@ -39,6 +58,21 @@ export default function MainPage(props) {
             ))}
           </List>
         </div>
+      )}
+      {button && (
+        <Fab
+          onClick={() => setButton(false)}
+          className={classes.fab}
+          aria-label="like"
+          color="secondary"
+        >
+          <FavoriteIcon />
+        </Fab>
+      )}
+      {!button && (
+        <Fab className={classes.fab} disabled>
+          Спасибо за лайк
+        </Fab>
       )}
     </div>
   );
